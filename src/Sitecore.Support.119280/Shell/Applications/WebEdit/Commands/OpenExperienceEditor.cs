cac309c6b3a940fd60c55e7a430c457d63c62ec9
Evgen Savinov <evs@sitecore.net>
2017-06-07 12:12:55 +0300

    changed a logic of the patch to resolve the current site according to the current host name if possible

--- src/Sitecore.Support.119280/Shell/Applications/WebEdit/Commands/OpenExperienceEditor.cs
+++ src/Sitecore.Support.119280/Shell/Applications/WebEdit/Commands/OpenExperienceEditor.cs
@@ -15,111 +15,126 @@ using Sitecore.Web.UI.Sheer;
 using System;
 using System.Collections.Specialized;
 using System.Globalization;
+using Sitecore.Web;
 
 namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
 {
-  
-
-  [Serializable]
-  public class OpenExperienceEditor : Command
-  {
-    public override void Execute(CommandContext context)
+    [Serializable]
+    public class OpenExperienceEditor : Command
     {
-      Assert.ArgumentNotNull(context, "context");
-      NameValueCollection parameters = new NameValueCollection();
-      bool flag = false;
-      if (context.Items.Length == 1)
-      {
-        Item item = context.Items[0];
-        parameters["uri"] = item.Uri.ToString();
-        parameters.Add("sc_lang", item.Language.ToString());
-        parameters.Add("sc_version", item.Version.Number.ToString(CultureInfo.InvariantCulture));
-        if (HasPresentationPipeline.Run(item))
+        public override void Execute(CommandContext context)
         {
-          parameters.Add("sc_itemid", item.ID.ToString());
+            Assert.ArgumentNotNull(context, "context");
+            NameValueCollection parameters = new NameValueCollection();
+            bool flag = false;
+            if (context.Items.Length == 1)
+            {
+                Item item = context.Items[0];
+                parameters["uri"] = item.Uri.ToString();
+                parameters.Add("sc_lang", item.Language.ToString());
+                parameters.Add("sc_version", item.Version.Number.ToString(CultureInfo.InvariantCulture));
+                if (HasPresentationPipeline.Run(item))
+                {
+                    parameters.Add("sc_itemid", item.ID.ToString());
+                }
+                else
+                {
+                    flag = true;
+                }
+            }
+            ClientPipelineArgs args = new ClientPipelineArgs(parameters);
+            if (!flag)
+            {
+                args.Result = "yes";
+                args.Parameters.Add("needconfirmation", "false");
+            }
+            Context.ClientPage.Start(this, "Run", args);
         }
-        else
-        {
-          flag = true;
-        }
-      }
-      ClientPipelineArgs args = new ClientPipelineArgs(parameters);
-      if (!flag)
-      {
-        args.Result = "yes";
-        args.Parameters.Add("needconfirmation", "false");
-      }
-      Context.ClientPage.Start(this, "Run", args);
-    }
 
-    public override CommandState QueryState(CommandContext context)
-    {
-      Assert.ArgumentNotNull(context, "context");
-      if ((!UIUtil.IsIE() || (UIUtil.GetBrowserMajorVersion() >= 7)) && Settings.WebEdit.Enabled)
-      {
-        return base.QueryState(context);
-      }
-      return CommandState.Hidden;
-    }
+        public override CommandState QueryState(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if ((!UIUtil.IsIE() || (UIUtil.GetBrowserMajorVersion() >= 7)) && Settings.WebEdit.Enabled)
+            {
+                return base.QueryState(context);
+            }
+            return CommandState.Hidden;
+        }
 
-    protected void Run(ClientPipelineArgs args)
-    {
-      Assert.ArgumentNotNull(args, "args");
-      if (SheerResponse.CheckModified())
-      {
-        if ((args.Parameters["needconfirmation"] == "false") || args.IsPostBack)
+        protected void Run(ClientPipelineArgs args)
         {
-          if (args.Result != "no")
-          {
-            UrlString str = new UrlString("/");
-            str.Add("sc_mode", "edit");
-            if (!string.IsNullOrEmpty(args.Parameters["sc_itemid"]))
+            Assert.ArgumentNotNull(args, "args");
+            if (SheerResponse.CheckModified())
             {
-              str.Add("sc_itemid", args.Parameters["sc_itemid"]);
+                if ((args.Parameters["needconfirmation"] == "false") || args.IsPostBack)
+                {
+                    if (args.Result != "no")
+                    {
+                        UrlString str = new UrlString("/");
+                        str.Add("sc_mode", "edit");
+                        if (!string.IsNullOrEmpty(args.Parameters["sc_itemid"]))
+                        {
+                            str.Add("sc_itemid", args.Parameters["sc_itemid"]);
+                        }
+                        if (!string.IsNullOrEmpty(args.Parameters["sc_version"]))
+                        {
+                            str.Add("sc_version", args.Parameters["sc_version"]);
+                        }
+                        SiteContext previewSiteContext = null;
+                        if (!string.IsNullOrEmpty(args.Parameters["uri"]))
+                        {
+                            Item item = Database.GetItem(ItemUri.Parse(args.Parameters["uri"]));
+                            if (item == null)
+                            {
+                                SheerResponse.Alert("Item not found.", new string[0]);
+                                return;
+                            }
+                            previewSiteContext = LinkManager.GetPreviewSiteContext(item);
+                        }
+                        //fix for 119280. If the current item is null and previewSiteContext has not been resolved, we try to resolve site context by using current host name only
+                        if (previewSiteContext == null)
+                        {
+                            System.Collections.Generic.List<SiteInfo> sites = SiteContextFactory.Sites;
+                            foreach (SiteInfo current in sites)
+                            {
+                                if (current.HostName.ToLowerInvariant().Equals(WebUtil.GetRequestUri().Host.ToLowerInvariant()))
+                                {
+                                    previewSiteContext = new SiteContext(current);
+                                    break;
+                                }
+                            }
+                        }
+                        SiteContext site = previewSiteContext ?? Factory.GetSite(Settings.Preview.DefaultSite);
+                        if (site == null)
+                        {
+                            object[] parameters = new object[] {Settings.Preview.DefaultSite};
+                            SheerResponse.Alert(Translate.Text("Site \"{0}\" not found", parameters), new string[0]);
+                        }
+                        else
+                        {
+                            string str2 = args.Parameters["sc_lang"];
+                            if (string.IsNullOrEmpty(str2))
+                            {
+                                str2 = WebEditUtility.ResolveContentLanguage(site).ToString();
+                            }
+                            if (!string.IsNullOrEmpty(args.Parameters["sc_lang"]))
+                            {
+                                str.Add("sc_lang", str2);
+                            }
+                            str["sc_site"] = site.Name;
+
+                            PreviewManager.RestoreUser();
+                            Context.ClientPage.ClientResponse.Eval("window.open('" + str + "', '_blank')");
+                        }
+                    }
+                }
+                else
+                {
+                    SheerResponse.Confirm(
+                        "The current item does not have a layout for the current device.\n\nDo you want to open the start Web page instead?");
+                    args.WaitForPostBack();
+                }
             }
-            if (!string.IsNullOrEmpty(args.Parameters["sc_version"]))
-            {
-              str.Add("sc_version", args.Parameters["sc_version"]);
-            }
-            SiteContext previewSiteContext = null;
-            if (!string.IsNullOrEmpty(args.Parameters["uri"]))
-            {
-              Item item = Database.GetItem(ItemUri.Parse(args.Parameters["uri"]));
-              if (item == null)
-              {
-                SheerResponse.Alert("Item not found.", new string[0]);
-                return;
-              }
-              previewSiteContext = LinkManager.GetPreviewSiteContext(item);
-            }
-            SiteContext site = previewSiteContext ?? Factory.GetSite(Settings.Preview.DefaultSite);
-            if (site == null)
-            {
-              object[] parameters = new object[] { Settings.Preview.DefaultSite };
-              SheerResponse.Alert(Translate.Text("Site \"{0}\" not found", parameters), new string[0]);
-            }
-            else
-            {
-              string str2 = args.Parameters["sc_lang"];
-              if (string.IsNullOrEmpty(str2))
-              {
-                str2 = WebEditUtility.ResolveContentLanguage(site).ToString();
-              }
-              if (!string.IsNullOrEmpty(args.Parameters["sc_lang"]))
-              {
-                str.Add("sc_lang", str2);
-              }
-              PreviewManager.RestoreUser();
-              Context.ClientPage.ClientResponse.Eval("window.open('" + str + "', '_blank')");
-            }
-          }
         }
-        else
-        {
-          SheerResponse.Confirm("The current item does not have a layout for the current device.\n\nDo you want to open the start Web page instead?");
-          args.WaitForPostBack();
-        }
-      }
     }
-  }
 }
